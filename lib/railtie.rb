d116f74219a29406bb6c01a1cf5e8a7c8d1e314a
Renae Bair <renaebair@gmail.com>
2012-01-10 21:50:32 -0500

    Rails 3 functionality

---
+++ lib/railtie.rb
@@ -0,0 +1,14 @@
+require 'canonical_url'
+
+module CanonicalURL
+
+  class Railtie < Rails::Railtie
+    initializer "canonical_url.action_controller" do
+      ActionController::Base.send :include, CanonicalURL::ControllerExtensions
+    end
+
+    initializer "canonical_url.action_view" do
+      ActionView::Base.send :include, CanonicalURL::Helpers
+    end
+  end
+end
